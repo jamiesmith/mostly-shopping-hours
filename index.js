530376ce57aeb2361e1a16d4bd99f42a22d07199
Jamie Smith <github@jamieandamy.com>
2017-11-01 16:59:59 -0400

    can't use const in strict

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var weekend = require('is-it-weekend')
 
-const HOUR_WEIGHTS = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.7, 0.8, 0.5, 1.0, 1.0, 1.0, 0.7, 0.5, 0.7, 0.8, 0.9, 0.9, 0.9, 0.7, 0.2, 0.1]
+var HOUR_WEIGHTS = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.7, 0.8, 0.5, 1.0, 1.0, 1.0, 0.7, 0.5, 0.7, 0.8, 0.9, 0.9, 0.9, 0.7, 0.2, 0.1]
 
 module.exports = function (cb) {
   var odds = HOUR_WEIGHTS[(new Date()).getHours()] * (weekend() ? 2 : 1)
