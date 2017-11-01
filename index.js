65835ce53b18ea07759730e7c1b8708847e6c83c
Jamie Smith <github@jamieandamy.com>
2017-11-01 16:47:49 -0400

    initial revision

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var weekend = require('is-it-weekend')
+
+const HOUR_WEIGHTS = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.7, 0.8, 0.5, 1.0, 1.0, 1.0, 0.7, 0.5, 0.7, 0.8, 0.9, 0.9, 0.9, 0.7, 0.2, 0.1]
+
+module.exports = function (cb) {
+  var odds = HOUR_WEIGHTS[(new Date()).getHours()] * (weekend() ? 2 : 1)
+  if (odds > Math.random()) {
+    cb()
+  }
+}
